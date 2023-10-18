62d283f0473cc044a4c7ca44fb04fded737f270e
Fredy Renteria <fredy.renteria@gmail.com>
2023-10-17 21:08:48 -0600

    added listing controller to server

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask_app import app
 
-from flask_app.controllers import users
+from flask_app.controllers import users,ListingController
 
 if __name__=="__main__":
     app.run(debug=True, host="localhost", port=8000)
