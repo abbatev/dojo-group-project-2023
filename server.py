88a9e1e3bb7c72f76ef2959a113a79d3dfea595a
Victor Acevedo <127136674+victoracevedojr@users.noreply.github.com>
2023-10-19 02:23:31 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask_app import app
 
-from flask_app.controllers import users,ListingController
+from flask_app.controllers import listings, users
 
 if __name__=="__main__":
     app.run(debug=True, host="localhost", port=8000)
